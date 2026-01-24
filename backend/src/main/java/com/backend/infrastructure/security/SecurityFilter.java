b7d0a18ae0b4691512d8eff11e45a742c937d313
AquilaDavid <aquiladavid07@gmail.com>
2026-01-24 20:54:02 -0300

    style: Mudança na organização do codigo

--- backend/src/main/java/com/backend/infrastructure/security/SecurityFilter.java
+++ backend/src/main/java/com/backend/infrastructure/security/SecurityFilter.java
@@ -17,43 +17,55 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-    @Autowired
-    TokenService tokenService;
 
-    @Autowired
-    UserRepository repository;
+@Autowired
+TokenService tokenService;
 
-    //metodo que executa a validacao do token
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        String token = recoverToken(request);
+@Autowired
+UserRepository repository;
 
-        if (token != null) {
-            String login = tokenService.validateToken(token);
 
-            UserDetails user = repository.findByEmail(login)
-                    .orElseThrow(() ->
-                            new RuntimeException("Usuário não encontrado")
-                    );
+@Override
+protected void doFilterInternal(
+HttpServletRequest request,
+HttpServletResponse response,
+FilterChain filterChain
+) throws ServletException, IOException {
 
-            var authentication =
-                    new UsernamePasswordAuthenticationToken(
-                            user,
-                            null,
-                            user.getAuthorities()
-                    );
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
+String token = recoverToken(request);
 
-        filterChain.doFilter(request, response);
-    }
 
-    //metodo que recupera o token
-    public String recoverToken(HttpServletRequest request) {
-        var authHeader = request.getHeader("Authorization");
-        if(authHeader == null) return null;
-        return authHeader.replace("Bearer ", "");
-    }
+if (token != null) {
+String login = tokenService.validateToken(token);
+
+
+UserDetails user = repository.findByEmail(login)
+.orElseThrow(() ->
+new RuntimeException("Usuário não encontrado")
+);
+
+
+var authentication =
+new UsernamePasswordAuthenticationToken(
+user,
+null,
+user.getAuthorities()
+);
+
+
+SecurityContextHolder.getContext().setAuthentication(authentication);
+}
+
+
+filterChain.doFilter(request, response);
+}
+
+
+private String recoverToken(HttpServletRequest request) {
+var authHeader = request.getHeader("Authorization");
+if (authHeader == null) return null;
+return authHeader.replace("Bearer ", "");
+}
 }
