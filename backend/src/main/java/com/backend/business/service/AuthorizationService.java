2b21a94da55cb4d09733c3dd5243210e1f4ab042
AquilaDavid <aquiladavid07@gmail.com>
2026-01-24 21:27:08 -0300

    fix: remoção de imports desnecessarios

--- backend/src/main/java/com/backend/business/service/AuthorizationService.java
+++ backend/src/main/java/com/backend/business/service/AuthorizationService.java
@@ -1,12 +1,10 @@
 package com.backend.business.service;
 
-import com.backend.business.mappers.UserMapper;
 import com.backend.infrastructure.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
