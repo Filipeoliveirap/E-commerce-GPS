b1dee0d2c3488ab03a60a7bc119565c22a983579
AquilaDavid <aquiladavid07@gmail.com>
2026-01-24 21:02:33 -0300

    feat: classe responsavel por transforma a entidade em Details

---
+++ backend/src/main/java/com/backend/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.backend.infrastructure.security;
+
+import com.backend.infrastructure.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        return repository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuário não encontrado"));
+    }
+}
