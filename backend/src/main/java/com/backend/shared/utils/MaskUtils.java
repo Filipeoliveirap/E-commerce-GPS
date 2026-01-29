7a2bafa01202ed0c8df7034b9a6e6ed17e2aff6d
AquilaDavid <aquiladavid07@gmail.com>
2026-01-28 23:55:00 -0300

    style: Ajuste nos espaçamentos tornamado mais legivel

--- backend/src/main/java/com/backend/shared/utils/MaskUtils.java
+++ backend/src/main/java/com/backend/shared/utils/MaskUtils.java
@@ -2,17 +2,20 @@ package com.backend.shared.utils;
 
 public class MaskUtils {
     public static String maskcpf(String cpf) {
-        if (cpf == null || cpf.length() != 11) return cpf;
+        if (cpf == null || cpf.length() != 11)
+            return cpf;
         return "***.***.***-" + cpf.substring(9);
     }
 
     public static String maskTelephone(String telephone) {
-        if (telephone == null || telephone.length() != 11) return telephone;
-        return telephone.substring(0,2) + " *****-" + telephone.substring(7);
+        if (telephone == null || telephone.length() != 11)
+            return telephone;
+        return telephone.substring(0, 2) + " *****-" + telephone.substring(7);
     }
 
     public static String maskEmail(String email) {
-        if (email == null || !email.contains("@")) return email;
+        if (email == null || !email.contains("@"))
+            return email;
 
         String[] parts = email.split("@");
         String nome = parts[0];
